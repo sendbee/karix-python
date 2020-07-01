f7df92d44a3c20c56c1315ee08f98c110b738cd6
Ivan Arar <ivan.arar@gmail.com>
2020-07-01 19:35:41 +0200

    create template

--- karix/api/whatsapp_api.py
+++ karix/api/whatsapp_api.py
@@ -166,6 +166,89 @@ class WhatsappApi(object):
             _request_timeout=params.get('_request_timeout'),
             collection_formats=collection_formats)
 
+    def create_whatsapp_template(self, **kwargs):  # noqa: E501
+        kwargs['_return_http_data_only'] = True
+        if kwargs.get('async_req'):
+            return self.create_whatsapp_template_with_http_info(**kwargs)  # noqa: E501
+        else:
+            (data) = self.create_whatsapp_template_with_http_info(**kwargs)  # noqa: E501
+            return data
+
+    def create_whatsapp_template_with_http_info(self, **kwargs):  # noqa: E501
+        all_params = ['api_version', 'category', 'whatsapp_account_uid',
+                      'name', 'language_code', 'attachment', 'text']  # noqa: E501
+        all_params.append('async_req')
+        all_params.append('_return_http_data_only')
+        all_params.append('_preload_content')
+        all_params.append('_request_timeout')
+
+        params = locals()
+        for key, val in six.iteritems(params['kwargs']):
+            if key not in all_params:
+                raise TypeError(
+                    "Got an unexpected keyword argument '%s'"
+                    " to method create_whatsapp_template" % key
+                )
+            params[key] = val
+        del params['kwargs']
+
+        collection_formats = {}
+
+        path_params = {}
+
+        query_params = []
+
+        header_params = {}
+        if 'api_version' in params:
+            header_params['api-version'] = params['api_version']  # noqa: E501
+
+        form_params = []
+        local_var_files = {}
+
+        body_params = None
+        if 'category' in params:
+            body_params = params['category']
+        if 'whatsapp_account_uid' in params:
+            body_params = params['whatsapp_account_uid']
+        if 'name' in params:
+            body_params = params['name']
+        if 'language_code' in params:
+            body_params = params['language_code']
+        if 'attachment' in params:
+            body_params = params['attachment']
+        if 'text' in params:
+            body_params = params['text']
+
+        print(body_params)
+        return
+
+        # HTTP header `Accept`
+        header_params['Accept'] = self.api_client.select_header_accept(
+            ['application/json'])  # noqa: E501
+
+        # HTTP header `Content-Type`
+        header_params['Content-Type'] = self.api_client.select_header_content_type(  # noqa: E501
+            ['application/json'])  # noqa: E501
+
+        # Authentication setting
+        auth_settings = ['basicAuth']  # noqa: E501
+
+        return self.api_client.call_api(
+            '/whatsapp/template/', 'POST',
+            path_params,
+            query_params,
+            header_params,
+            body=body_params,
+            post_params=form_params,
+            files=local_var_files,
+            response_type='InlineResponse2014',  # noqa: E501
+            auth_settings=auth_settings,
+            async_req=params.get('async_req'),
+            _return_http_data_only=params.get('_return_http_data_only'),
+            _preload_content=params.get('_preload_content', True),
+            _request_timeout=params.get('_request_timeout'),
+            collection_formats=collection_formats)
+
     def get_whatsapp_accounts(self, **kwargs):  # noqa: E501
         kwargs['_return_http_data_only'] = True
         if kwargs.get('async_req'):
